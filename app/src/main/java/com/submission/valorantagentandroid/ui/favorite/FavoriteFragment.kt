c05f51ef460f11aa8e7a183a06a05c237084e0de
Airi <airiagustus82@gmail.com>
2024-08-31 04:29:01 +0700

    Change fragment names and adjust bottom navigation label visibility

---
+++ app/src/main/java/com/submission/valorantagentandroid/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,40 @@
+package com.submission.valorantagentandroid.ui.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.submission.valorantagentandroid.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val favoriteViewModel =
+            ViewModelProvider(this)[FavoriteViewModel::class.java]
+
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        favoriteViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/submission/valorantagentandroid/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/submission/valorantagentandroid/ui/favorite/FavoriteViewModel.kt
@@ -1,10 +1,10 @@
-package com.submission.valorantagentandroid.ui.dashboard
+package com.submission.valorantagentandroid.ui.favorite
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class FavoriteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
