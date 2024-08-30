32259ea5a4bc5005faacaa46116a721866a06c1b
Airi <airiagustus82@gmail.com>
2024-08-31 04:13:18 +0700

    SplashScreen support dark mode

--- app/src/main/java/com/submission/valorantagentandroid/SplashScreen.kt
+++ app/src/main/java/com/submission/valorantagentandroid/SplashScreen.kt
@@ -2,29 +2,48 @@ package com.submission.valorantagentandroid
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.submission.valorantagentandroid.databinding.ActivitySplashScreenBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        checkDarkMode()
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@SplashScreen, MainActivity::class.java)
             startActivity(intent)
             finish()
         }, 2500)
+    }
+
+    private fun checkDarkMode() {
+        val isDarkModeActive =
+            (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES)
+        if (isDarkModeActive) {
+            binding.ivSplashScreen.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        } else {
+            binding.ivSplashScreen.setColorFilter(ContextCompat.getColor(this, R.color.black))
+        }
     }
 }
